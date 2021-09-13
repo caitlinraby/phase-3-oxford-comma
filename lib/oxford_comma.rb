f6a3599f0129ed4d94dfe32bb170e5538d289e30
Caitlin Raby <caitlinraby@gmail.com>
2021-09-13 13:15:58 -0500

    commit tree 9bab7c1179474b8a20364b4f20a3050535441936

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
-end
+    if array.length == 1
+        array.join
+    elsif array.length > 1 && array.length < 3
+        array.join(" and ")
+    elsif array.length > 2
+        def remove_last(array)
+        array.pop
+        end
+        last_one = remove_last(array)
+        array.join(", ") + ", and #{last_one}"
+    end
+  end
